18a8241536564536a1b998012494133ae31efc79
Kornimate <kornimate@gmail.com>
2025-10-10 03:13:59 +0200

    fixed tabs

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -59,7 +59,7 @@ export function Home() {
           }
         });
       },
-      { root: null, rootMargin: "-20% 0px -60% 0px", threshold: 0 }
+      { root: null, rootMargin: "0px 0px -60% 0px", threshold: 0.25 }
     );
 
     sections.forEach((s) => {
